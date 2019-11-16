1ddbd557a708517dc5c93f60fec735b094be13b7
Senthilnathan Ravichandran <Senthilnathan.Ravichandran@nasdaq.com>
2019-11-16 17:37:39 +0530

    Widget module changes

--- src/components/stock/stock.module.ts
+++ src/components/stock/stock.module.ts
@@ -1,5 +1,5 @@
 //import angular from 'angular';
-import './stock.component'
+import StockComponent from "./stock.component"
 
 // Creating widget module and registering widget component
 angular.module("stockModule", []).component("stockWidget", StockComponent)
